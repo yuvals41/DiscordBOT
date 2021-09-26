ada36b5a7bd9f478f585a0ffa27d17c16c11aba7
yuvals41 <yuvals41@gmail.com>
2021-09-26 22:58:48 +0300

    update

---
+++ experiment.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from time import sleep
+from datetime import date
+import discord
+
+time = date.day
+print(time)
---
+++ test.py
@@ -0,0 +1,2 @@
+with open('TwitterLinks_A17.txt','a',encoding = 'utf-8') as file:
+   file.write('https://twitter.com/Kef_64/status/1425832072128516113'+'\n')
