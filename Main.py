a312b94b3a2a8bcb0fb764ccd9d3e237a05cda5b
yuvals41 <yuvals41@gmail.com>
2021-09-26 22:37:31 +0300

    update

--- Main.py
+++ Main.py
@@ -1,18 +1,27 @@
 import discord
 
-
 client = discord.Client()
+
+
+async def send_Link(filename, channel, history):
+    flag = True
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            async for message in history:
+                if(message.content + "\n" == line or message.content == line):
+                    flag = False
+                    break
+            if(flag == True):
+                await channel.send(line)
+            flag = True
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await send_Link("TwitterLinks_A17.txt", client.get_channel(888376010519740457), client.get_channel(888376010519740457).history(limit=None))
+    await send_Link("TwitterLinks_GokuBlack.txt", client.get_channel(890631861099970631), client.get_channel(890631861099970631).history(limit=None))
+    await send_Link("TwitterLinks_GokuBlue.txt", client.get_channel(890634130390409256), client.get_channel(890634130390409256).history(limit=None))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello Nigga!')
-
-client.run('')
+client.run('ODg4MzczNjAyNzQxODAwOTcy.YURwZA.5n40mNFc1C9bnh9eNJPKQ8IhiHk')
